57515ed79dd8b4d7a842a90be5ede19b76122839
Ratul Hossain <ratulhosainbnc@gmamil.com>
2025-07-01 21:40:36 +0600

    added categories in constants.js

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -1,1 +1,20 @@
 export const VITE_APP_URL = "http://localhost:5173";
+
+export const categories = [
+  { label: "Home Services", value: "home-services" },
+  { label: "Cleaning & Maintenance", value: "cleaning-and-maintenance" },
+  { label: "Health & Wellness", value: "health-and-wellness" },
+  { label: "Beauty & Personal Care", value: "beauty-and-personal-care" },
+  { label: "Tutoring & Education", value: "tutoring-and-education" },
+  { label: "Technology & IT Support", value: "technology-and-it-support" },
+  { label: "Event & Party Services", value: "event-and-party-services" },
+  { label: "Automotive Services", value: "automotive-services" },
+  { label: "Pet Care", value: "pet-care" },
+  { label: "Fitness & Training", value: "fitness-and-training" },
+  { label: "Legal & Financial Services", value: "legal-and-financial-services" },
+  { label: "Business Support", value: "business-support" },
+  { label: "Photography & Videography", value: "photography-and-videography" },
+  { label: "Freelance & Creative Services", value: "freelance-and-creative-services" },
+  { label: "Repair & Handyman", value: "repair-and-handyman" },
+  { label: "Travel & Hospitality", value: "travel-and-hospitality" },
+];
